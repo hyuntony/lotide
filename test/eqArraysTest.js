364337cb3cfe70bef275fc5985f5c9e1fb2de17e
hyuntony <ankim90@gmail.com>
2021-05-31 13:40:55 -0400

    added eqArrayTest.js

---
+++ test/eqArraysTest.js
@@ -0,0 +1,7 @@
+const {assertEqual, eqArrays} = require('../eqArrays');
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
