3d5fbad3e54090613b74e746157590f53d183ac3
hyuntony <ankim90@gmail.com>
2021-06-08 07:28:04 -0400

    added recursion for eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,16 @@
-const assertEqual = require('./assertEqual');
-
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
   }
   for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
+    if (Array.isArray(array1[i])) {
+      if (!eqArrays(array1[i], array2[i])) {
+        return false;
+      }
+    } else if (!Array.isArray(array1[i])) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
     }
   }
   return true;
